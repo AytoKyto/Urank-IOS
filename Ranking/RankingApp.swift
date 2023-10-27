74fb3e71927b190bf6a44b71dc4c2ec8a12a6ae3
fleury_mathis <mathis@lanageuse.com>
2023-10-27 22:00:22 +0200

    Initial Commit

---
+++ Ranking/RankingApp.swift
@@ -0,0 +1,17 @@
+//
+//  RankingApp.swift
+//  Ranking
+//
+//  Created by Mathis Fleury on 27/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct RankingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
