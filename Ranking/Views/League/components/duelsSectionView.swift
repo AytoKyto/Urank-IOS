0e42dd0748731bfdcd4bc8bd11b810ed9f904325
fleury_mathis <mathis@lanageuse.com>
2024-03-12 21:36:18 +0100

    add detail league

---
+++ Ranking/Views/League/components/duelsSectionView.swift
@@ -0,0 +1,18 @@
+//
+//  duelsSectionView.swift
+//  Ranking
+//
+//  Created by Mathis Fleury on 29/01/2024.
+//
+
+import SwiftUI
+
+struct duelsSectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    duelsSectionView()
+}
---
+++ Ranking/Views/League/components/playersSectionView.swift
@@ -0,0 +1,18 @@
+//
+//  playersSectionView.swift
+//  Ranking
+//
+//  Created by Mathis Fleury on 29/01/2024.
+//
+
+import SwiftUI
+
+struct playersSectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    playersSectionView()
+}
